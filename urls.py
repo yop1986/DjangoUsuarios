80e57ed57d78d78d44abfdb61b8df62d57c85ccc
Pablo Godoy <pablodavid36@gmail.com>
2023-07-16 23:15:51 -0600

    feat(usuarios): plantilla base para los ususarios y barra de navegacion

--- urls.py
+++ urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 
+from .views import Home, Index
+
+app_name = 'usuarios'
+
 urlpatterns = [
     #path('usuarios/', include('usuarios.urls')),
+    path('', Home.as_view(), name='home'),
+    path('usuarios/', Index.as_view(), name='index'),
 
-    path('cuentas/', include('django.contrib.auth.urls')),
+    path('usuarios/', include('django.contrib.auth.urls')),
 ]
--- views.py
+++ views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.utils.translation import gettext as _
+from django.views.generic.base import ContextMixin, TemplateView
 
-# Create your views here.
+# Configuración del sitio web
+class PersonalContextMixin(ContextMixin):
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['config'] = settings.CONFIGURACION
+        return context 
+
+# PRIVATE VIEWS
+
+
+# PUBLIC VIEWS
+class Home(TemplateView, PersonalContextMixin):
+    template_name = 'home.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = context['config']['sitio']
+        context['apps'] = self.get_installed_apps()
+        return context
+
+    def get_installed_apps(self):
+        mis_apps = []
+        for app, info in settings.APPS_DESC.items():
+            if app in settings.INSTALLED_APPS:
+                mis_apps.append(info)
+
+        return mis_apps
+
+
+class Index(TemplateView, PersonalContextMixin):
+    template_name = 'index.html'
+    extra_context = {
+        'title': _('Usuarios'),
+    }
